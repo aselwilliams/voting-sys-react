65a61768548db9bd79804b1aee6c7d978c4d20fd
aselwilliams <aselwilliams22@gmail.com>
2022-05-11 12:18:10 -0400

    teams data file added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-
+import { Component } from 'react';
 import './App.css';
+import data from './data.js'
 
-function App() {
-  return (
-    <div className="App">
-   
+class App extends Component{
+  constructor() {
+    super()
+    this.state={
+      teams: data
+    }
+  }
+
+render() {
+return(
+  <div class="container">
+    <div class="card">
+        <img src="https://seeklogo.com/images/F/fc-barcelona-rugby-logo-8F77CA5AD8-seeklogo.com.png" alt="barcelona logo" />
+        <h3>F.C. Barcelona</h3>
+        <button id="button1">Vote</button>
+        <button type="reset" id="clear1">Clear</button>
+    <div class="lower">
+        <p>Vote count: <span id="span1">0</span></p>
     </div>
-  );
+    </div>
+    </div>
+)
 }
-
+}
 export default App;
---
+++ src/data.js
@@ -0,0 +1,17 @@
+const data = [
+    {
+        name:'F.C. Barcelona',
+        img:"https://seeklogo.com/images/F/fc-barcelona-rugby-logo-8F77CA5AD8-seeklogo.com.png",
+        vote: 0,
+    },
+    {
+        name:'Juventus F.C.',
+        img:"http://media-s3-us-east-1.ceros.com/bwin/images/2019/04/22/9e67c4305b108cb26d522c24bb063c18/43852-13545869-2529999-f55338a3-image.png",
+        vote: 0,
+    },
+    {
+        name: 'Manchester United F.C.',
+        img:"https://brandlogos.net/wp-content/uploads/2012/12/manchester-united-logo-vector-200x200.png",
+        vote: 0,
+    }
+]
